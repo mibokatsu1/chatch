16c4fd7dad77e63e624ea00ccbb11c7cf9ceeee3
mibokatsu1 <mibo.katsu@gmail.com>
2020-06-23 17:50:35 +0900

    Create message-model

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,6 @@ class Group < ApplicationRecord
   has_many :group_users
   # group.usersの記述で呼び出せる
   has_many :users, through: :group_users
+  has_many :messages
   validates :name, presence: true, uniqueness: true
 end
