8526ab4f4315e86f9906715410862b2916619f87
mibokatsu1 <mibo.katsu@gmail.com>
2020-06-23 11:28:20 +0900

    group and user_group migrate

--- db/migrate/20200622132307_create_groups.rb
+++ db/migrate/20200622132307_create_groups.rb
@@ -2,7 +2,7 @@ class CreateGroups < ActiveRecord::Migration[6.0]
   def change
     create_table :groups do |t|
       t.string :name, null: false
-      t.index :name, unipue: true
+      t.index :name, unique: true
       t.timestamps
     end
   end
