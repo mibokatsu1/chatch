cf3f363967ee4dccbf740edc724c449374455239
mibokatsu1 <mibo.katsu@gmail.com>
2020-06-26 13:41:12 +0900

    Link show.html
    Rewrite show-action

--- app/controllers/all_users_chats_controller.rb
+++ app/controllers/all_users_chats_controller.rb
@@ -1,18 +1,18 @@
 class AllUsersChatsController < ApplicationController
   before_action :set_all_users_chat, only: :show
   def index
-    # @all_users_chats = All_users_chat.all
+    @all_users_chats = AllUsersChat.all
   end
   def new
     @all_users_chat = AllUsersChat.new
   end
   def create
-    
+
     AllUsersChat.create(all_users_chat_params)
   end
   def show
-    @all_users_chat = Comment.new
-    @all_users_chats = @all_users_chat.comments.includes(:user)
+    @comment = Comment.new
+    @comments = @all_users_chat.comments.includes(:user)
   end
   def destroy
   end
