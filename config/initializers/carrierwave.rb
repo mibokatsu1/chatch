bfe10fce313e05d500e59aaf2808e7fbbf625dfd
mibokatsu1 <mibo.katsu@gmail.com>
2020-07-25 01:46:20 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = ‘chatch’
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatch’
+  config.fog_directory  = 'chatch'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatch'
 end
