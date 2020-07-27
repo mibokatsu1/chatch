27fb7f71340e7c1f79f81498161a7cb39660b5ab
mibokatsu1 <mibo.katsu@gmail.com>
2020-07-27 23:37:54 +0900

    create migration-file about Tag

--- db/migrate/20200727140402_create_all_users_chat_tags.rb
+++ db/migrate/20200727140402_create_all_users_chat_tags.rb
@@ -1,6 +1,6 @@
-class CreateAllUsersChatsTags < ActiveRecord::Migration[6.0]
+class CreateAllUsersChatTags < ActiveRecord::Migration[6.0]
   def change
-    create_table :all_users_chats_tags do |t|
+    create_table :all_users_chat_tags do |t|
       t.references :all_users_chat, foreign_key: true
       t.references :tag, foreign_key: true
       t.timestamps
